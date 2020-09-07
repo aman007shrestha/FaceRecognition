6a48a18b4687e63d744bc728c673e290a1476812
aman <KAN007BCT@student.kec.edu.np>
2020-09-07 11:48:30 +0545

    Some changes (september 5)

--- face_detection/training_2.py
+++ face_detection/training_2.py
@@ -40,55 +40,59 @@ for root, dirs, files in os.walk(image_dir):
             # print(id_) gives id as in 0 or 1
             pil_image = Image.open(path).convert('L')
             # directly converts to grayscale pil_image is actually the grayscale image not pixel intenity
-            size = (400, 400)
-            final_image = pil_image.resize(size, Image.ANTIALIAS)
+            # size = (100, 100)
+            # final_image = pil_image.resize(size, Image.ANTIALIAS)
+            # print(final_image)
             #  Antialias refers to reducing distortion while converting high resolution image to low
-            image_array = np.array(final_image, 'uint8')
-            # print(image_array) numpy array in grayscale
-            faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
-            for (x, y, w, h) in faces:
-                cv2.rectangle(image_array, (x, y), (x+w, y+h), (0, 255, 0),3 )
-
-                roi = image_array[y:y + h, x:x + w]
-                # roi = cv2.resize(roi, (100, 100)) roi is cropped image after face detection
-                # print(roi.shape)
-                # im = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)
-                # plt.imshow(im)
-                # plt.show()
-
-
-
-                x_train.append(roi)
-                y_labels.append(id_)
-
+            image_array = np.array(pil_image, 'uint8')
+            # print(image_array.shape)
+#             # print(image_array) numpy array in grayscale
+#             faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
+#             for (x, y, w, h) in faces:
+#                 cv2.rectangle(image_array, (x, y), (x+w, y+h), (0, 255, 0),3 )
+#
+#                 roi = image_array[y:y + h, x:x + w]
+#                 # roi = cv2.resize(roi, (100, 100)) roi is cropped image after face detection
+#                 # print(roi.shape)
+#                 # im = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)
+#                 # plt.imshow(im)
+#                 # plt.show()
+#
+#
+#
+            x_train.append(image_array)
+            y_labels.append(id_)
+            #  This is visualization bits we can delete it
             i = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
             plt.imshow(i)
             plt.show()
-
-
-# print(x_train)   contains the list of numpy array of detected cropped faces
-# print(len(x_train))
-# print(y_labels)  contains the label of detected face corresponding to x_train
-# print(len(y_labels))
-# lets examine the first data from set x_train
-# analyze = x_train[0]
-# print(analyze.shape)
-# print(analyze[150])  # lets check the data in 150th row
-
-#[  0   0   0 191 191 191 192 194 194 194 193 193 193 194 194 194 194 194
-#perfectly fine
-
+            # upto here
+#
+#
+#
+# # print(x_train)   contains the list of numpy array of detected cropped faces
+# # print(len(x_train))
+# # print(y_labels)  contains the label of detected face corresponding to x_train
+# # print(len(y_labels))
+# # lets examine the first data from set x_train
+# # analyze = x_train[0]
+# # print(analyze.shape)
+# # print(analyze[150])  # lets check the data in 150th row
+#
+# #[  0   0   0 191 191 191 192 194 194 194 193 193 193 194 194 194 194 194
+# #perfectly fine
+#
 
 with open("labels.pickle", 'wb') as f:
     pickle.dump(label_ids, f)
-
+#
 recognizer.train(x_train, np.array(y_labels))
-# print(help(recognizer.train))
+# # print(help(recognizer.train))
 recognizer.save("trainner.yml")
-
-
-    # print(root, dirs, files)
-# C:\Users\aman1\minor project jupyter\Face recog 1\TrainingData ['Emilia Clarke', 'Peter Dinklage'] []
-# C:\Users\aman1\minor project jupyter\Face recog 1\TrainingData\Emilia Clarke [] ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg']
-# C:\Users\aman1\minor project jupyter\Face recog 1\TrainingData\Peter Dinklage [] ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg']
+#
+#
+#     # print(root, dirs, files)
+# # C:\Users\aman1\minor project jupyter\Face recog 1\TrainingData ['Emilia Clarke', 'Peter Dinklage'] []
+# # C:\Users\aman1\minor project jupyter\Face recog 1\TrainingData\Emilia Clarke [] ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg']
+# # C:\Users\aman1\minor project jupyter\Face recog 1\TrainingData\Peter Dinklage [] ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg']
 
